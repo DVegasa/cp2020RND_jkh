42816cacca2dee443340350a80dff2b32c9b445a
Ed Khalturin <DracVeg@gmail.com>
2020-08-22 12:30:41 +0300

    Пинкод

--- app/src/main/java/io/github/dvegasa/cp2020rnd/features/voting_screen/VotingFragment.kt
+++ app/src/main/java/io/github/dvegasa/cp2020rnd/features/voting_screen/VotingFragment.kt
@@ -1,11 +1,18 @@
 package io.github.dvegasa.cp2020rnd.features.voting_screen
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
+import com.poovam.pinedittextfield.PinField
 import io.github.dvegasa.cp2020rnd.R
+import kotlinx.android.synthetic.main.fragment_voting.*
+import kotlinx.android.synthetic.main.fragment_voting.view.*
+
 
 class VotingFragment : Fragment() {
 
@@ -17,7 +24,33 @@ class VotingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_voting, container, false)
+        val view = inflater.inflate(R.layout.fragment_voting, container, false)
+
+        view.apply {
+            btnSmsRequest.setOnClickListener {
+                btnSmsRequest.visibility = View.GONE
+                pinField.visibility = View.VISIBLE
+                tvInfo.text = "Введите код из СМС-сообщения\nВ демо-версии можно ввести любые цифры"
+
+                pinField.requestFocus()
+                val imm: InputMethodManager? =
+                    context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm!!.showSoftInput(pinField, InputMethodManager.SHOW_IMPLICIT)
+            }
+
+            pinField.onTextCompleteListener = object : PinField.OnTextCompleteListener {
+                override fun onTextComplete(enteredText: String): Boolean {
+                    pinCodeEntered()
+                    return true
+                }
+            }
+        }
+        return view
+    }
+
+    private fun pinCodeEntered() {
+        pinField.visibility = View.GONE
+        tvInfo.text = "Теперь вы можете принять участие в голосовании"
     }
 
     companion object {
