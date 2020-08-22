289078fddfc80896024799ed25520550d2f0f7ad
Ed Khalturin <DracVeg@gmail.com>
2020-08-22 08:23:30 +0300

    Кнопка баттомнава

--- app/src/main/java/io/github/dvegasa/cp2020rnd/MainActivity.kt
+++ app/src/main/java/io/github/dvegasa/cp2020rnd/MainActivity.kt
@@ -2,10 +2,20 @@ package io.github.dvegasa.cp2020rnd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadBottomNav()
+    }
+
+    private fun loadBottomNav() {
+        val view = LayoutInflater.from(this).inflate(R.layout.view_bottomnav_0, flBottomnavHolder, false)
+        flBottomnavHolder.removeAllViews()
+        flBottomnavHolder.addView(view)
     }
 }
