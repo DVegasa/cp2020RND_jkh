c3bc1f73f4171c183c7677cc93d13ebfaedab3a0
Ed Khalturin <DracVeg@gmail.com>
2020-08-22 08:43:55 +0300

    Теньь

---
+++ app/src/main/java/io/github/dvegasa/cp2020rnd/features/main_screen/MainFragment.kt
@@ -0,0 +1,30 @@
+package io.github.dvegasa.cp2020rnd.features.main_screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.dvegasa.cp2020rnd.R
+
+class MainFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
