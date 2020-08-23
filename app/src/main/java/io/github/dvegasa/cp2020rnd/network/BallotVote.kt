095ad449a69295a38939b620dfec176c7a9c0e04
Ed Khalturin <DracVeg@gmail.com>
2020-08-23 05:57:32 +0300

    работает

--- app/src/main/java/io/github/dvegasa/cp2020rnd/network/BallotVote.kt
+++ app/src/main/java/io/github/dvegasa/cp2020rnd/network/BallotVote.kt
@@ -3,21 +3,12 @@ package io.github.dvegasa.cp2020rnd.network
 
 import com.google.gson.annotations.SerializedName
 
-/*
-vote: 
-5f4164dfe8d1dc02e57f168a
-5f4164e4e8d1dc02e57f168b
-5f4164eae8d1dc02e57f168c
-5f416254ea0e89005887a838
- */
 
 data class BallotVote(
-    @SerializedName("id")
-    val id: String,
+    @SerializedName("pollId")
+    val pollId: String,
     @SerializedName("vote")
     val vote: String,
-    @SerializedName("user")
-    val user: String = "5f415965cb91bf003054217c",
-    @SerializedName("voteValue")
-    val voteValue: Int = 100
+    @SerializedName("userId")
+    val userId: String = "5f41d5fdabf767002bf8abe7"
 )
